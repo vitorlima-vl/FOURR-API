a288db9f9a179525f12548ec5ade9aa6a870742c
jtomaz99 <jtomaz1999@gmail.com>
2019-09-30 18:54:38 -0300

    Adicionando o arquivo de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :solicitacaos, :historicos, :produtos, :departamentos
+  devise_for :profiles
+  devise_for :admins
   get '/', to: 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post 'register', to: 'application#nome_do_metodo'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
