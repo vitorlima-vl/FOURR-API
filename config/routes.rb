433e32eb1d90165c59587c48f38333846bf651e1
jtomaz99 <jtomaz1999@gmail.com>
2019-09-30 18:56:09 -0300

    Atualizando o arquivo de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :profiles
-  devise_for :admins
+  resources :solicitacaos, :historicos, :produtos, :departamentos
   get '/', to: 'welcome#index'
 
   post 'register', to: 'application#nome_do_metodo'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
