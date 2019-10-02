3d8e0522660c86de5557637706edf5d436efdb37
ifsl2 <ifsl2@cin.ufpe.br>
2019-10-02 19:28:20 -0300

    Criando Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :solicitacaos, :historicos, :produtos, :departamentos
-  get '/', to: 'welcome#index'
-
-  post 'register', to: 'application#nome_do_metodo'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :users
+  get    'sign_in'   => 'login#new'
+  post   'sign_in'   => 'login#create'
+  delete 'sign_out'  => 'login#destroy'
+  end
