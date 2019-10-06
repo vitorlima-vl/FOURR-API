8b88b2fb288eb1807f4095a4051366b2e57fbb70
Vitor Lima <vals@cin.ufpe.br>
2019-10-06 20:24:33 -0300

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,5 @@ module FourrApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.api_only = true
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
