a3f03978aedc0b3d8282f96e86d4d0d1a4fe28c8
Vitor Lima <vals@cin.ufpe.br>
2019-10-06 20:56:25 -0300

    fix session assign2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 					.try(:authenticate, params["departamento"]["password"])
 
 		if departamento
-			#session[:user_id] = user_id
+			session[:user_id] = user.id
 			render json: {
 				status: :created,
 				logged_in: true,
