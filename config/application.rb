be67b7932cd9c42d9647e8bcd984cd64a7c0d11e
vitorlima-vl <vals@cin.ufpe.br>
2019-10-07 22:50:27 -0300

    fixing cookies-store

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module FourrApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.api_only = true
+	config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::Cookies
+	config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::Session::CookieStore
   end
 end
