a6d612e2d66679be288ae0b9b83553b564eede64
Vitor Lima <vals@cin.ufpe.br>
2019-11-03 12:54:12 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete :logout, to: "sessions#logout"
 
   get :logged_in, to: "sessions#logged_in"
-  post :search, to: "produtos#buscar"
+  get :search, to: "produtos#buscar"
 
   post :forgot, to: "senhas#forgot"
   post :reset, to: "senhas#reset"
